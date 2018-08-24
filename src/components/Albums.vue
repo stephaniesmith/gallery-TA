4051d286c3497f7cd3b854e24e632863598ddf17
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-24 07:50:06 -0700

    created basic albums component

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,12 +1,24 @@
 <template>
-  <h3 class="albums">
-    Albums
-  </h3>
+  <ul class="albums">
+    <li 
+      v-for="album in albums"
+      :key="album.id"
+    >
+    {{album.title}}
+    </li>
+  </ul>
 </template>
 
 <script>
+import albumsApi from '../services/albumsApi.js';
+
 export default {
   name: 'Albums',
+  data() {
+    return {
+      albums: albumsApi.getAlbums()
+    }
+  }
 }
 </script>
 
