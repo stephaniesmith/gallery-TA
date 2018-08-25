857d20fe7456fb9b44efe64fd23d9c99d57dc013
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-24 19:12:40 -0700

    called api to get album and setup sub router

--- src/components/AlbumDetail.vue
+++ src/components/AlbumDetail.vue
@@ -1,17 +1,35 @@
 <template>
-  <h3 class="detail">
-    Album Detail
-  </h3>
+  <div>
+    <h3 class="detail">
+      {{ album.title }}
+    </h3>
+
+    <nav>
+      <router-link to="thumbnail">Thumbail</router-link>
+      <router-link to="gallery">Gallery</router-link>
+      <router-link to="list">List</router-link>
+    </nav>
+
+    <router-view :album="album"></router-view>
+  </div>
 </template>
 
 <script>
+import albumsApi from '../services/albumsApi'
+
 export default {
-  name: 'Album Detail',
+  name: 'AlbumDetail',
+  data() {
+    return {
+      album: null
+    }
+  },
+  created() {
+    this.album = albumsApi.getAlbum(this.$route.params.id);
+  }
 }
 </script>
 
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
+
 </style>
