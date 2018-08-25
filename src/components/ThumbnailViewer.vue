9da00233b4bf52a139c1440a8e765bd0571bfcf1
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-24 19:34:12 -0700

    thumbnail view has cats

--- src/components/ThumbnailViewer.vue
+++ src/components/ThumbnailViewer.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    Thumbnail Viewer
+    <div class="thumbnail"
+      v-for="image in album.images"
+      :key="image.id"
+      :style="{ backgroundImage: 'url(' + image.url + ')' }"
+    > 
+    </div>
   </div>
 </template>
 
@@ -12,5 +17,14 @@
 </script>
 
 <style scoped>
-
+.thumbnail {
+  max-width: 400px;
+  max-height: 400px;
+  width: 75vw;
+  height: 75vw;
+  margin: 20px auto;
+  background-position: center;
+  background-repeat: none;
+  background-size: cover;
+}
 </style>
