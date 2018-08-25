07650279512d7e30d6838a6996152f15656a6cfa
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-24 21:41:50 -0700

    created form

---
+++ src/components/AlbumForm.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <p>
+      <label>
+        Title: <input v-modle="title">
+      </label>
+    <p>
+    <p>
+      <label>
+        Description: <input v-modle="description">
+      </label>
+    <p>
+    <p>
+      <button>add</button>
+    <p>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        id: '',
+        title: '',
+        description: '',
+      }
+    },
+    methods: {
+
+    }
+  },
+</script>
+
+<style scoped>
+
+</style>
