be6ff99b8189f3f0a7c8ac3a4831ed0797bd0bc3
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-25 10:25:53 -0700

    working on the form for adding a new image

--- src/components/NewImage.vue
+++ src/components/NewImage.vue
@@ -1,13 +1,54 @@
 <template>
-  <div class="new-image">
-    
-  </div>
+    <form @submit.prevent="handleSubmit">
+    <p>
+      <label>
+        Title: <input v-model="title">
+      </label>
+    </p>
+    <p>
+      <label>
+        Description: <input v-model="description">
+      </label>
+    <p>
+    <p>
+      <label>
+        Image URL: <input v-model="url">
+      </label>
+    <p>
+    <p>
+      <button>add</button>
+    </p>
+  </form>
 </template>
 
 <script>
+import albumsApi from '../services/albumsApi';
+
   export default {
     name: 'NewImage',
-    props: ['album']
+    props: ['album'],
+        data() {
+      return {
+        id: '',
+        title: '',
+        description: '',
+        url: ''
+      }
+    },
+    methods: {
+      handleSubmit() {
+        const image = {
+          title: this.title,
+          description: this.description,
+          url: this.url
+        }
+        this.onComplete(image);
+      },
+      handleAdd(image) {
+        albumsApi.postImage(image);
+        this.$router.push(`/albums/${this.album.id}`);
+      }
+    }
   }
 </script>
 
