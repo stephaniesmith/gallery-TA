9fc251ba6cf2ccac5787dffa75b8ea0bd1aa7827
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-08-24 21:14:54 -0700

    gallery view

--- src/components/GalleryViewer.vue
+++ src/components/GalleryViewer.vue
@@ -1,16 +1,37 @@
 <template>
-  <div>
-    Gallery Viewer
+  <div class="gallery">
+    <div>
+      <button @click="handleDecrease" :disabled="this.imageIndex === 0">prev</button>
+      <button @click="handleIncrease" :disabled="this.imageIndex === album.images.length - 1">next</button>
+    </div>
+    <img :src="album.images[imageIndex].url">
   </div>
 </template>
 
 <script>
   export default {
     name: 'GalleryViewer',
-    props: ['album']
+    props: ['album'],
+    data() {
+      return {
+        imageIndex: 0
+      }
+    },
+    methods:{
+      handleIncrease() {
+        this.imageIndex ++;
+      },
+      handleDecrease() {
+        this.imageIndex --;
+      }
+    }
   }
 </script>
 
 <style scoped>
+
+.gallery {
+  display: block;
+}
 
 </style>
